02a07f302743a40b758e51ce287865befd161705
Valentin <valentin.chigot@orange.fr>
2024-02-23 17:03:49 +0100

    Add files via upload

--- app/test/page.jsx
+++ app/test/page.jsx
@@ -1,18 +1,22 @@
-"use client"
-import { useState, useEffect } from 'react';
+// "use client"
+// import { useState, useEffect } from 'react';
+import './style.css';
 export default function Home() {
-    const [text, setText] = useState('');
-    const [isClicked, setIsClicked] = useState(false);
+    // const [text, setText] = useState('');
+    // const [isClicked, setIsClicked] = useState(false);
 
-    useEffect(() => {
-        console.log(text);
-        setIsClicked(false);
-    }, [isClicked]);
+    // useEffect(() => {
+    //     console.log(text);
+    //     setIsClicked(false);
+    // }, [isClicked]);
 
     return (
         <div>
-            <input className='text-black' type="text" value={text} onChange={e => setText(e.target.value)} />
-            <button onClick={() => setIsClicked(true)}>Valider</button>
+            <section className='la' style={{ color: "green" }}>
+                mape
+            </section>
+            {/* <input className='text-black' type="text" value={text} onChange={e => setText(e.target.value)} />
+            <button onClick={() => setIsClicked(true)}>Valider</button> */}
         </div>
     );
 }
